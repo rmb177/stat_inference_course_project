036a9e11d162dfe656a1f223dc54d2bb75f3b3b1
Ryan Brubaker <ryan.brubaker@gmail.com>
2014-08-23 23:42:40 -0500

    Most of the RMD files are in place. I just need to clean up the output and trim to two pages.

---
+++ toothGrowth.R
@@ -0,0 +1,9 @@
+library(datasets)
+
+vc5  <- ToothGrowth[1:10,  1]
+vc10 <- ToothGrowth[11:20, 1]
+vc20 <- ToothGrowth[21:30, 1]
+oj5  <- ToothGrowth[31:40, 1]
+oj10 <- ToothGrowth[41:50, 1]
+oj20 <- ToothGrowth[51:60, 1]
+
